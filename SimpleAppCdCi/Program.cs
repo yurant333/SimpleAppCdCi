02f89cd22e2e0b818ed322dbb900476ee0f15566
yurant333 <yurant333@gmail.com>
2023-11-07 02:11:14 +0400

    test

--- SimpleAppCdCi/Program.cs
+++ SimpleAppCdCi/Program.cs
@@ -1,6 +1,6 @@
 ﻿var builder = WebApplication.CreateBuilder();
 var app = builder.Build();
 
-app.MapGet("/", ()=>"Hi, Max! Do you remember me?");
+app.MapGet("/", ()=>"Hi, Maxx! Do you remember me?");
 
 app.Run();
